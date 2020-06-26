64e11189f89c4551881435287f7117034379fb6d
507134920 <18473095331@163.com>
2020-06-26 12:56:46 +0800

    init:项目初始结构构建

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimalProductApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimalProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimalProductApplication.class, args);
+    }
+
+}
